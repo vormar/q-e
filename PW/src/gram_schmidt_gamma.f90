573cf62af2f06d681abed22e93d57c3521c7b36e
nisihara1 <nisihara.burai@gmail.com>
2017-03-16 22:59:21 +0900

    set overlap for Gram-Schmidt.

--- PW/src/gram_schmidt_gamma.f90
+++ PW/src/gram_schmidt_gamma.f90
@@ -7,24 +7,71 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !--------------------------------------------------------------------------
-SUBROUTINE gram_schmidt_gamma( npwx, npw, nbnd, npol, psi, overlap )
+SUBROUTINE gram_schmidt_gamma( npwx, npw, nbnd, npol, psi, overlap, gstart, nblock )
   !--------------------------------------------------------------------------
   !
   ! ... Gram-Schmidt orthogonalization, for Gamma-only calculations.
   ! ... blocking algorithm is used.
   !
-  USE kinds, ONLY : DP
+  USE kinds,    ONLY : DP
+  USE mp,       ONLY : mp_sum
+  USE mp_bands, ONLY : intra_bgrp_comm
   !
   IMPLICIT NONE
   !
   ! ... I/O variables
   !
   INTEGER,     INTENT(IN)    :: npw, npwx, nbnd, npol
-  LOGICAL,     INTENT(IN)    :: overlap
   COMPLEX(DP), INTENT(INOUT) :: psi(npwx*npol,nbnd)
+  LOGICAL,     INTENT(IN)    :: overlap
+  INTEGER,     INTENT(IN)    :: gstart
+  INTEGER,     INTENT(IN)    :: nblock
+  !
+  ! ... local variables
+  !
+  INTEGER                  :: kdim, kdmx
+  COMPLEX(DP), ALLOCATABLE :: aux(:,:)
+  REAL(DP),    ALLOCATABLE :: sr(:,:)
+  !
+  ALLOCATE( aux( npwx, nbnd ) )
+  ALLOCATE( sr( nbnd, nbnd ) )
+  !
+  ! ... set Im[ psi(G=0) ] -  needed for numerical stability
+  !
+  IF ( gstart == 2 ) psi(1,1:nbnd) = CMPLX( DBLE( psi(1,1:nbnd) ), 0._DP, kind=DP )
+  !
+  ! ... Set up the Overlap matrix on the subspace :
+  !
+  ! ...      S_ij = <psi_i| S |psi_j>
+  !
+  IF ( overlap ) THEN
+     !
+     CALL s_psi( npwx, npw, nbnd, psi, aux )
+     !
+     CALL DGEMM( 'T', 'N', nbnd, nbnd, 2 * npw, 2._DP, psi, 2 * npwx, aux, 2 * npwx, 0._DP, sr, nbnd )
+     !
+     IF ( gstart == 2 ) CALL DGER( nbnd, nbnd, -1._DP, psi, 2 * npwx, aux, 2 * npwx, sr, nbnd )
+     !
+  ELSE
+     !
+     CALL DGEMM( 'T', 'N', nbnd, nbnd, 2 * npw, 2._DP, psi, 2 * npwx, psi, 2 * npwx, 0._DP, sr, nbnd )
+     !
+     IF ( gstart == 2 ) CALL DGER( nbnd, nbnd, -1._DP, psi, 2 * npwx, psi, 2 * npwx, sr, nbnd )
+     !
+  END IF
+  !
+  CALL mp_sum( sr , intra_bgrp_comm )
+  !
+
   !
   ! TODO
   ! TODO
   ! TODO
+  !
+
+  DEALLOCATE( aux )
+  DEALLOCATE( sr )
+  !
+  RETURN
   !
 END SUBROUTINE gram_schmidt_gamma
